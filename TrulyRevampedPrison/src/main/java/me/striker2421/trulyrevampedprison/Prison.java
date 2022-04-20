325e91c90350249b9ba89d6c2161905ae1ba78ce
Striker2421 <32222474+Striker2421@users.noreply.github.com>
2022-04-19 20:05:07 -0500

    Add files via upload

---
+++ TrulyRevampedPrison/src/main/java/me/striker2421/trulyrevampedprison/Prison.java
@@ -0,0 +1,17 @@
+package me.striker2421.trulyrevampedprison;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Prison extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
